58cec6f5403bb239959e8561bf3110d3e1155b78
Rahul A <rahul.a.rajan@gmail.com>
2020-03-15 04:37:04 +0000

    outputs.tf
    deployment username changed

--- outputs.tf
+++ outputs.tf
@@ -7,5 +7,5 @@ output "app_service_default_hostname" {
 }
 
 output "app_service_git_cloneurl" {
-  value = "rahulayyampully@${azurerm_app_service.itaca1.source_control.0.repo_url}:443/${azurerm_app_service.itaca1.name}.git"
+  value = "itaca1deployer@${azurerm_app_service.itaca1.source_control.0.repo_url}:443/${azurerm_app_service.itaca1.name}.git"
 }
